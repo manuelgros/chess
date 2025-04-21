90c7a95dfd2e140e6e0f8a414dc94562937d1c8b
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-21 14:26:55 -0700

    Renamed full_army var, deleted comments, create sorting methods for back and pawn rank

--- lib/chess_army.rb
+++ lib/chess_army.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'chess_army'
+require 'chess_piece'
 
 # Class for ChessArmy
 class ChessArmy
-  attr_reader :color, :full_army, :piece_database
+  attr_reader :color, :full_set, :piece_database
 
   def initialize(color)
     @color = color
@@ -16,19 +16,29 @@ class ChessArmy
       [:queen, 1, [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]], 7],
       [:king, 1, [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]], 1]
     ]
-    @full_army = create_full_army(@color, piece_database)
+    @full_set = create_full_set(@color, piece_database)
   end
 
   def create_chess_piece(color, array)
     ChessPiece.new(color, array)
   end
 
-  def create_full_army(color, database)
-    database.each_with_object({}) do |piece, army|
-      army[piece[0]] = Array.new(piece[1]) { create_chess_piece(color, piece) }
-      # for i in 1..piece[1]
-      #   army[piece[0]][i] = create_chess_piece(color, piece)
-      # end
+  def create_full_full_set(color, database)
+    database.each_with_object({}) do |piece, full_set|
+      full_set[piece[0]] = Array.new(piece[1]) { create_chess_piece(color, piece) }
     end
+  end
+
+  def sort_pawn_rank
+    @full_set[:pawn]
+  end
+
+  def sort_back_rank
+    [
+      full_set[:rook][0], full_set[:knight][0],
+      full_set[:bishop][0], full_set[:king][0],
+      full_set[:queen][0], full_set[:bishop][1],
+      full_set[:knight][1], full_set[:rook][1]
+    ]
   end
 end
