926abe68b505dfa496d9b4b2bb6069890a53bccc
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-21 09:15:27 -0700

    Created ChessArmy class

---
+++ lib/chess_army.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'chess_army'
+
+# Class for ChessArmy
+class ChessArmy
+  attr_reader :color, :full_army
+
+  def initialize(color)
+    @color = color
+    @chess_piece_database = {
+      pawn: [:pawn, 8, [[1, 0], [1, 1], [1, -1]], 1],
+      rook: [:rook, 2, [[1, 0], [-1, 0], [0, 1], [0, -1]], 7],
+      knights: [:knight, 2, [[2, 1], [2, -1], [1, 2], [1, -2]], 1],
+      bishops: [:bishop, 2, [[1, 1], [1, -1], [-1, 1], [-1, -1]], 7],
+      queen: [:queen, 1, [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]], 7],
+      king: [:king, 1, [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]], 1]
+    }
+    @full_army = create_full_army(chess_piece_database)
+  end
+
+  def create_chess_piece(array)
+    ChessPiece.new(array)
+  end
+
+  def create_full_army(hash)
+    hash.each_value_with_object([]) do |full_set, value|
+      value[1].times do
+        full_set << create_chess_piece(value)
+      end
+    end
+  end
+end
