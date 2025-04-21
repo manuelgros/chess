a508b6452a9f2086b63715a0d59adbce5fb13ce2
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-21 14:27:47 -0700

    Created method to populate Chessboard array with ChessPiece objects in correct order

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -5,4 +5,11 @@ require 'chess_board'
 require 'messages'
 
 # Module for Game Logic
-module GameLogic; end
+module GameLogic
+  def setting_up_ranks(player)
+    army = player.army
+    starting_rows = player.eql?(:white) ? [0, 1] : [7, 6]
+    @board[starting_rows[0]] = army.sort_back_rank
+    @board[starting_rows[1]] = army.sort_pawn_rank
+  end
+end
