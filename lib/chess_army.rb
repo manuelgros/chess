85d23bc79cd33490065859845affffd207b2785c
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-21 13:49:19 -0700

    Changed the data structure for Chess_army class. Changed the database structure to nested arrays and the return of the create_full_army method to hash, containing the various piece objects, sorted by type. This should make accessing them easier and more intuitive.

--- lib/chess_army.rb
+++ lib/chess_army.rb
@@ -4,30 +4,31 @@ require 'chess_army'
 
 # Class for ChessArmy
 class ChessArmy
-  attr_reader :color, :full_army
+  attr_reader :color, :full_army, :piece_database
 
   def initialize(color)
     @color = color
-    @chess_piece_database = {
-      pawn: [:pawn, 8, [[1, 0], [1, 1], [1, -1]], 1],
-      rook: [:rook, 2, [[1, 0], [-1, 0], [0, 1], [0, -1]], 7],
-      knights: [:knight, 2, [[2, 1], [2, -1], [1, 2], [1, -2]], 1],
-      bishops: [:bishop, 2, [[1, 1], [1, -1], [-1, 1], [-1, -1]], 7],
-      queen: [:queen, 1, [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]], 7],
-      king: [:king, 1, [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]], 1]
-    }
-    @full_army = create_full_army(@color, chess_piece_database)
+    @piece_database = [
+      [:pawn, 8, [[1, 0], [1, 1], [1, -1]], 1],
+      [:rook, 2, [[1, 0], [-1, 0], [0, 1], [0, -1]], 7],
+      [:knight, 2, [[2, 1], [2, -1], [1, 2], [1, -2]], 1],
+      [:bishop, 2, [[1, 1], [1, -1], [-1, 1], [-1, -1]], 7],
+      [:queen, 1, [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]], 7],
+      [:king, 1, [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]], 1]
+    ]
+    @full_army = create_full_army(@color, piece_database)
   end
 
-  def create_chess_piece(array)
-    ChessPiece.new(array)
+  def create_chess_piece(color, array)
+    ChessPiece.new(color, array)
   end
 
-  def create_full_army(color, hash)
-    hash.each_value_with_object([]) do |full_set, value|
-      value[1].times do
-        full_set << create_chess_piece(color, value)
-      end
+  def create_full_army(color, database)
+    database.each_with_object({}) do |piece, army|
+      army[piece[0]] = Array.new(piece[1]) { create_chess_piece(color, piece) }
+      # for i in 1..piece[1]
+      #   army[piece[0]][i] = create_chess_piece(color, piece)
+      # end
     end
   end
 end
