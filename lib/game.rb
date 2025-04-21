bd4f354e65cf0b0011299d8484a222eefa38ab91
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-21 14:31:56 -0700

    Created method for basic set up of the game

--- lib/game.rb
+++ lib/game.rb
@@ -7,11 +7,17 @@ require 'game_logic'
 
 # Class for Game Logic
 class Game
-  include GameLogic `
-  `
+  include GameLogic
+  attr_reader :board, :player_one, :player_two
+
   def initialize
     @board = ChessBoard.new
     @player_one = Player.new(1, :white)
     @player_two = Player.new(2, :black)
+  end
+
+  def setting_up_game
+    setting_up_ranks(@player_one)
+    setting_up_ranks(@player_two)
   end
 end
