b010473df278f3c751bec1c56a82a7d822fb074a
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-21 09:15:13 -0700

    Created ChessPiece class

---
+++ lib/chess_piece.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Class for single ChessPiece
+class ChessPiece
+  attr_reader :type, :movement, :range
+
+  def initialize(array)
+    @type = array[0]
+    @movement = array[2]
+    @range = array[3]
+  end
+end
