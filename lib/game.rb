8f549d557d095329054dee44b4ea45de45d0913a
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-23 16:24:14 -0700

    Modified and moved setting_up_ranks method to Game class and adjusted the setting_up method. GameLogic module stays unused for now

--- lib/game.rb
+++ lib/game.rb
@@ -16,8 +16,17 @@ class Game
     @player_two = Player.new(:black)
   end
 
-  def setting_up_game
-    setting_up_ranks(@player_one)
-    setting_up_ranks(@player_two)
+  def setting_up_ranks(player)
+    sorted_army = player.army.sort_ranks_for_start
+    # returns array with sorted pieces; major rank is idex 0 to 7, pawns 8 to 16
+    starting_rows = player.color.eql?(:white) ? [0, 1] : [7, 6]
+    @board[starting_rows[0]] = sorted_army[0..7]
+    @board[starting_rows[1]] = sorted_army[8..15]
+  end
+
+  def setup_board
+    setting_up_ranks(player_one)
+    setting_up_ranks(player_two)
+    puts board
   end
 end
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -5,11 +5,4 @@ require_relative '../lib/chess_board'
 require_relative '../lib/messages'
 
 # Module for Game Logic
-module GameLogic
-  def setting_up_ranks(player)
-    army = player.army
-    starting_rows = player.eql?(:white) ? [0, 1] : [7, 6]
-    @board[starting_rows[0]] = army.sort_back_rank
-    @board[starting_rows[1]] = army.sort_pawn_rank
-  end
-end
+module GameLogic; end
