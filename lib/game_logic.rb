c8b910075116713692dc9e8a9662f277fce7f590
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-29 18:34:22 -0700

    Implemented simple method to move piece on game board.

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -5,4 +5,11 @@ require_relative '../lib/chess_board'
 require_relative '../lib/messages'
 
 # Module for Game Logic
-module GameLogic; end
+module GameLogic
+  
+  def move_piece(select_arr, dest_arr)
+    @board[dest_arr[0]][dest_arr[1]] = @board[select_arr[0]][select_arr[1]]
+    @board[select_arr[0]][select_arr[1]] = nil
+  end
+  
+end
