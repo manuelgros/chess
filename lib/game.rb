cf32cef198c5d63b0d9c086db6984abd6fb94ad0
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-29 17:33:33 -0700

    Added reference to active board , and moved setup_ranks method into Player class for better organization

--- lib/game.rb
+++ lib/game.rb
@@ -12,21 +12,13 @@ class Game
 
   def initialize
     @board = Array.new(8) { Array.new(8) }
-    @player_one = Player.new(:white)
-    @player_two = Player.new(:black)
-  end
-
-  def setting_up_ranks(player)
-    sorted_army = player.army.sort_ranks_for_start
-    # returns array with sorted pieces; major rank is idex 0 to 7, pawns 8 to 16
-    starting_rows = player.color.eql?(:white) ? [0, 1] : [7, 6]
-    @board[starting_rows[0]] = sorted_army[0..7]
-    @board[starting_rows[1]] = sorted_army[8..15]
+    @player_one = Player.new(:white, @board)
+    @player_two = Player.new(:black, @board)
   end
 
   def setup_board
-    setting_up_ranks(player_one)
-    setting_up_ranks(player_two)
+    player_one.setup_ranks
+    player_two.setup_ranks
     puts board
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -8,9 +8,10 @@ class Player
   include Messages
   attr_reader :name, :color, :army
 
-  def initialize(color)
+  def initialize(color, board)
     @color = color
     @name = ask_player_name
+    @board = board
     @army = ChessArmy.new(color)
   end
 
@@ -18,4 +19,13 @@ class Player
     print player_messages(:get_name)
     gets.chomp
   end
+
+  def setup_ranks
+    sorted_army = @army.sort_ranks_for_start
+    # returns array with sorted pieces; major rank is idex 0 to 7, pawns 8 to 16
+    starting_rows = @color.eql?(:white) ? [0, 1] : [7, 6]
+    @board[starting_rows[0]] = sorted_army[0..7]
+    @board[starting_rows[1]] = sorted_army[8..15]
+  end
+
 end
