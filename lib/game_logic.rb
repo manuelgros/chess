a3aa0eda3fd7e05dd4c4bd3c1daf059537eb9d6f
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-29 19:04:28 -0700

    Minor changes

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -6,10 +6,8 @@ require_relative '../lib/messages'
 
 # Module for Game Logic
 module GameLogic
-  
   def move_piece(select_arr, dest_arr)
     @board[dest_arr[0]][dest_arr[1]] = @board[select_arr[0]][select_arr[1]]
     @board[select_arr[0]][select_arr[1]] = nil
   end
-  
 end
