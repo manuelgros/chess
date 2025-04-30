439395afe83cfb6c639313962ee0e200b84febf5
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-04-29 17:32:16 -0700

    Renamed instance variable

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -3,10 +3,11 @@
 # Chess_board class
 class ChessBoard
   def initialize
-    @square = create_board_squares
+    @board = create_board_squares
   end
 
   def create_board_squares
     Array.new(8) { Array.new(8) }
   end
+
 end
