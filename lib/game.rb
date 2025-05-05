943519df2d6994077406e2e70d69fa732e66356d
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-05 13:41:54 -0700

    Added variable to store player for current turn. Added method names that are still need appropriate content

--- lib/game.rb
+++ lib/game.rb
@@ -8,17 +8,26 @@ require_relative '../lib/game_logic'
 # Class for Game Logic
 class Game
   include GameLogic
-  attr_reader :board, :player_one, :player_two
+  attr_reader :board, :player_one, :player_two, :current_player
 
   def initialize
     @board = Array.new(8) { Array.new(8) }
     @player_one = Player.new(:white, @board)
     @player_two = Player.new(:black, @board)
+    @current_player = @player_one
   end
 
   def setup_board
     player_one.setup_ranks
     player_two.setup_ranks
     puts board
+  end
+
+  def single_turn
+    # script for a full player turn
+  end
+
+  def full_match
+    # script to run a full match until end conditions are met (win, draw, capitulation etc.)
   end
 end
