fcaefe18fe20d02b6a4db8183a4d29500a5139bd
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-10 10:15:35 -0700

    Simplified method names

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -12,7 +12,7 @@ class ChessPiece
     @board = board
   end
 
-  def find_position
+  def position
     @board.squares.each_with_index do |row, y_coord|
       x_coord = row.index(self)
       return [y_coord, x_coord] if x_coord
@@ -20,16 +20,16 @@ class ChessPiece
     nil
   end
 
-  # Takes array as parameter to determine destination on board array of ChessBoard
-  def move_piece(destination)
-    current_position = find_position
+  # Takes array with coordinates. Determines destination on board array of ChessBoard
+  def move(destination)
+    current_position = position
     @board.squares[destination[0]][destination[1]] = @board.squares[current_position[0]][current_position[1]]
     @board.squares[current_position[0]][current_position[1]] = nil
   end
 
   # Takes a direction as array (exp. [1, 0] = one square up) and calculates the full range by returning
   # array with all valid fields that could be selected for the destination for certain piece
-  def calc_range(current_position, direction)
+  def reach(current_position, direction)
     full_range = []
     @range.times do
       current_position = [
@@ -42,11 +42,11 @@ class ChessPiece
     full_range
   end
 
-  # Takes all directions arrays from @movement and calls calc_range for each
-  def find_paths
-    current_position = find_position
+  # Takes all directions arrays from @movement and calls range for each
+  def direction
+    current_position = position
     @movement.each_with_object([]) do |direction, valid_moves|
-      valid_moves << calc_range(current_position, direction)
+      valid_moves << reach(current_position, direction)
       valid_moves.flatten!(1)
     end
   end
--- lib/main.rb
+++ lib/main.rb
@@ -8,5 +8,10 @@ require_relative '../lib/game'
 # Run code her
 game = Game.new
 game.setup_board
-puts "#{game.board[7][0].color} #{game.board[7][0].type}"
-puts "#{game.board[0][0].color} #{game.board[0][0].type}"
+puts "#{game.board.select_square([7, 0]).color} #{game.board.select_square([7, 0]).type}"
+puts "#{game.board.select_square([0, 0]).color} #{game.board.select_square([0, 0]).type}"
+pp game.board.select_square([1, 0]).position
+pp game.board.select_square([1, 0]).direction
+game.board.select_square([1, 0]).move([2, 0])
+puts game.board.select_square([2, 0])
+puts game.board.select_square([1, 0])
