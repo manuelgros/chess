5761d0c3101ca3ab4f20bf20a4914741716764e6
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-15 18:37:10 -0700

    Reordered message keys to the appropriate hashes and debugged method

--- lib/messages.rb
+++ lib/messages.rb
@@ -5,20 +5,21 @@ require_relative '../lib/player'
 # Message module
 module Messages
   def validate_coordinate_input(input_arr)
-    input_arr.size == 2 && input_arr.all?(&Integer.method(:===))
+    @board.includes_coordinates?(input_arr) && input_arr.all?(&Integer.method(:===))
   end
 
   def player_messages(message)
     {
       get_name: "Type in name for #{@color} Player: ",
       get_selection: 'Select piece by coordinated: ',
-      get_destination: 'Select filed to move the piece to: '
+      get_destination: 'Select filed to move the piece to: ',
+      coord_input_error: 'Invalid input. Please type coordinates on the X and Y axis between 0 and 7 (example: 24)'
     }[message]
   end
 
   def game_messages(message)
     {
-      coord_input_error: 'Invalid input. Please type coordinates on the X and Y axis between 0 and 7 (example: 24)'
+      capture: "#{piece.color} #{piece.type} captures #{destination.color} #{destination.type}"
     }[message]
   end
 end
