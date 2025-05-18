86dd9e0c583215d9bfd94b4d4ef546e89806e703
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-17 17:37:20 -0700

    Moved methods to get piece selection and destination to Game class. Adjusted method for player so that it just generally gets coordinates which then can be used for any selection

--- lib/player.rb
+++ lib/player.rb
@@ -20,33 +20,32 @@ class Player
     gets.chomp
   end
 
-  def select_piece
-    print player_messages(:get_selection)
+  def select_coordinates
     selection = gets.chomp.chars.map(&:to_i)
-    puts selection
-    if validate_coordinate_input(selection) && !@board.select_square(selection).nil?
+    if validate_coordinate_input(selection) # && !@board.select_square(selection).nil?
       selection
     else
       puts player_messages(:coord_input_error) # confusing, need better messages for different scenarios
-      select_piece
+      select_coordinates
     end
   end
 
-  def select_destination
-    print player_messages(:get_destination)
-    selection = gets.chomp.chars.map(&:to_i)
-    return selection if validate_coordinate_input(selection)
+  # moved to game.rb
+  # def select_destination
+  #   print player_messages(:get_destination)
+  #   selection = gets.chomp.chars.map(&:to_i)
+  #   return selection if validate_coordinate_input(selection)
 
-    puts player_messages(:coord_input_error)
-    select_destination
-  end
+  #   puts player_messages(:coord_input_error)
+  #   select_destination
+  # end
 
-  def setup_ranks
-    # sort_ranks_for_start brings army array into right order
-    # returns array with sorted pieces; major rank is idex 0 to 7, pawns 8 to 16
-    sorted_army = @army.sort_ranks_for_start
-    starting_rows = @color.eql?(:white) ? [0, 1] : [7, 6] # decides side of board depending on color
-    @board.squares[starting_rows[0]] = sorted_army[0..7] # major rank (root, knight etc.)
-    @board.squares[starting_rows[1]] = sorted_army[8..15] # pawn rank
-  end
+  # def setup_ranks
+  #   # sort_ranks_for_start brings army array into right order
+  #   # returns array with sorted pieces; major rank is idex 0 to 7, pawns 8 to 16
+  #   sorted_army = @army.sort_ranks_for_start
+  #   starting_rows = @color.eql?(:white) ? [0, 1] : [7, 6] # decides side of board depending on color
+  #   @board.squares[starting_rows[0]] = sorted_army[0..7] # major rank (root, knight etc.)
+  #   @board.squares[starting_rows[1]] = sorted_army[8..15] # pawn rank
+  # end
 end
