6b9d5f7d19745f455eddb82b0b7cc733cd333901
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-19 19:17:04 -0700

    Refactored method to be used with constant instead of case statements

--- lib/rendering.rb
+++ lib/rendering.rb
@@ -4,6 +4,21 @@ require 'colorize'
 
 # Module to render the chess game for display in console
 module Rendering
+  SKINS = {
+    rook: ' R ',
+    knight: ' K ',
+    bishop: ' B ',
+    queen: ' Q ',
+    king: ' K ',
+    pawn: ' P '
+  }.freeze
+
+  def translate(item)
+    return '   ' if item.nil?
+
+    SKINS[item.type]
+  end
+
   def display_board
     @board.squares.each_with_index do |row, row_index|
       print "#{row_index}  ".colorize(:yellow)
@@ -11,18 +26,5 @@ module Rendering
       puts "\n"
     end
     puts '    0  1  2  3  4  5  6  7'.colorize(:yellow)
-  end
-
-  def translate(item)
-    return '   ' if item.nil?
-
-    case item.type
-    when :rook then ' R '
-    when :knight then ' K '
-    when :bishop then ' B '
-    when :queen then ' Q '
-    when :king then ' H '
-    when :pawn then ' P '
-    end
   end
 end
