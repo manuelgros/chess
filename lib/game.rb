60aa2cbe0380ecbf16ff15bfd43605c6c54f2d8e
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-19 19:06:46 -0700

    Added Rendering module to Game class and implemented it.

--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,12 @@
 require_relative '../lib/player'
 require_relative '../lib/chess_board'
 require_relative '../lib/game_communication'
+require_relative '../lib/rendering'
 
 # Class for Game Logic
 class Game
   include GameCommunication
+  include Rendering
 
   attr_reader :board, :player_one, :player_two
   attr_accessor :current_player
@@ -30,6 +32,7 @@ class Game
     if active_piece.valid_moves.include?(target)
       active_piece.move(target)
       puts move_messages(:move, active_piece, target)
+      display_board
       return
     end
 
@@ -40,6 +43,7 @@ class Game
 
   def full_match
     # script to run a full match until end conditions are met (win, draw, capitulation etc.)
+    display_board
     until check_mate?
       puts game_messages(:new_turn)
       turn
