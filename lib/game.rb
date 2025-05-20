3f6764407d4db1e6f316df20b0bfbb3a35fde287
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-19 18:10:21 -0700

    Implemented script method for single turn. Still needs adjustment for capture case.

--- lib/game.rb
+++ lib/game.rb
@@ -23,20 +23,19 @@ class Game
     puts board
   end
 
-  # def single_turn
-  #   selected_piece = @board.select_square(current_player.select_piece)
-  #   unless selected_piece.any_moves?
-  #     puts 'No moves for that piece'
-  #     single_turn
-  #   end
-  #   target = current_player.select_destination
-  #   # puts game_messages(:capture) unless @board.select_square(destination).nil?
-  #   if selected_piece.valid_moves.include?(target)
-  #     selected_piece.move(target)
-  #   else
-  #     puts
-  #   end
-  # end
+  def turn
+    active_piece = current_player.select_piece
+    target = current_player.select_destination
+    if active_piece.valid_moves.include?(target)
+      active_piece.move(target)
+      puts move_messages(:move, active_piece, target)
+      return
+    end
+
+    puts move_messages(:invalid_move, active_piece, target)
+
+    turn
+  end
 
   def full_match
     # script to run a full match until end conditions are met (win, draw, capitulation etc.)
