2417b68ce9fb2835ad6dfb3d5778c079b088edb1
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-25 15:13:50 -0700

    Implemented background coloring for the squares of the board

--- lib/rendering.rb
+++ lib/rendering.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 require 'colorize'
+require 'colorized_string'
 
 # Module to render the chess game for display in console
 module Rendering
-  MARKER = {
-    rook: ' R ',
-    knight: ' N ',
-    bishop: ' B ',
-    queen: ' Q ',
-    king: ' K ',
-    pawn: ' P '
-  }.freeze
+  # Different version of black side pieces
+  # BLACK_SIDE = {
+  #   king: " \u265a ".colorize(:red),
+  #   queen: " \u265b ".colorize(:red),
+  #   rook: " \u265c ".colorize(:red),
+  #   bishop: " \u265d ".colorize(:red),
+  #   knight: " \u265e ".colorize(:red),
+  #   pawn: " \u265f ".colorize(:red)
+  # }.freeze
 
   BLACK_SIDE = {
     king: " \u2654 ",
@@ -41,15 +43,37 @@ module Rendering
     end
   end
 
+  def color_square(field_color, field)
+    return ColorizedString[field].on_white if field_color == :white
+
+    ColorizedString[field].on_black
+  end
+
+  def display_row(field_color, row)
+    row.each do |field|
+      symbol = translate(field)
+      print color_square(field_color, symbol)
+      field_color = field_color == :white ? :black : :white
+    end
+
+    puts "\n"
+  end
+
   def display_board
+    field_color = :black
+    @board.squares.reverse.each_with_index do |row, row_index|
+      print "#{(row_index - 7).abs}  ".colorize(:yellow)
+      display_row(field_color, row)
+      field_color = field_color == :white ? :black : :white
+    end
+    puts '    0  1  2  3  4  5  6  7'.colorize(:yellow)
+  end
+
+  def simple_display_board
     @board.squares.reverse.each_with_index do |row, row_index|
       print "#{(row_index - 7).abs}  ".colorize(:yellow)
       row.each do |field|
-        if field.nil? || field.color == :white
-          print translate(field)
-        else
-          print translate(field).colorize(:light_black)
-        end
+        print translate(field)
       end
       puts "\n"
     end
