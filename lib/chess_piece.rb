499bdc30977f0f6fe42603dd2abbc28764eebd60
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-27 16:06:04 -0700

    Multiple Changes
    1. Added King class with some early draft methods
    
    2. Adjusted Pawn class and implemented custom getter methods for
    range and movement to address the special movement options for Pawn
    
    3. Early drafts for check?() to see if a move would cause the King
    to be in check
    
    4. Added more 'state' methods, to determine the enemy color or the
    player in charge of a piece(commander()). check?() required it at this
    point. Will circle back though and see if better solution is available

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -8,7 +8,7 @@ class ChessPiece
     @color = color
     @board = board
     @type = type
-    @movement = database[:moves]
+    @movement = database[:move_direction]
     @range = database[:range]
   end
 
@@ -25,8 +25,18 @@ class ChessPiece
     @color == :white ? :black : :white
   end
 
-  def enemy?
-    @color == opponent_color
+  def opponent_army
+    @board.squares.select do |piece|
+      piece.color == opponent_color
+    end
+  end
+
+  def commander
+    @board.game.player_one.color == @color ? @board.game.player_one : @board.game.player_two
+  end
+
+  def enemy?(target)
+    target.color == opponent_color
   end
 
   def any_moves?
@@ -45,6 +55,7 @@ class ChessPiece
     start = position
     @board.change_square(destination, self)
     @board.change_square(start, EmptySquare.new)
+    destination
   end
 
   # METHOD TO DETERMINE VALID MOVEMENTS DURING TURN - BEGIN
@@ -61,25 +72,38 @@ class ChessPiece
   # takes direction (exp. [1, 0] -> up) and returns all reachable squares, based on position() and @range
   def reach(direction)
     position = position()
+    reachable = []
 
-    (1..@range).each_with_object([]) do |_, reachable|
+    range.times do
       position = next_square(position, direction)
       break unless valid_coord?(position)
 
       target = @board.select_square(position)
 
-      next reachable << position if target.type == :empty
-
-      reachable << position if target.enemy?
-      break
+      if target.type == :empty
+        reachable << position
+      else
+        reachable << position if enemy?(target)
+        break
+      end
     end
+    reachable
   end
 
   # returns array with all reachable squares in all possible directions
   def valid_moves
-    @movement.each_with_object([]) do |direction, valid_moves|
+    movement.each_with_object([]) do |direction, valid_moves|
       valid_moves.concat(reach(direction))
     end
+  end
+
+  # check if moving the piece would cause Player to be in check
+  def cause_check?
+    current_position = position
+    @board.change_square(current_position, EmptySquare.new)
+    result = commander.check?
+    @board.change_square(current_position, self)
+    result
   end
   # METHOD TO DETERMINE VALID MOVEMENTS DURING TURN - END
 end
@@ -88,36 +112,72 @@ end
 class Pawn < ChessPiece
   def initialize(color, board, type, database)
     super
-    @movement = database[:moves][@color][:regular]
-    @capture_moves = database[:moves][@color][:capture_moves]
+    @movement = database[:move_direction][@color]
+    # @movement = database[:move_direction][@color][:move]
+    # @capture = database[:move_direction][@color][:capture]
     @first_move = true
+    @range = database[:range]
   end
 
   # returns additional directions IF field is occupied by opponent piece
-  def attack_moves
-    @capture_moves.select do |attack_direction|
+  # BUG array contains same coord multiple times, and it seem to grow the more capture moves a Pawn takes
+  def attack_moves(direction)
+    direction.select do |attack_direction|
       target_coord = next_square(position, attack_direction)
       next unless valid_coord?(target_coord)
 
       target = @board.select_square(target_coord)
-      target.enemy?
+      enemy?(target)
     end
   end
 
-  def reach(direction)
-    @range = @first_move ? 2 : 1 # change to use range data from database
-    super(direction)
+  def range
+    @first_move ? @range[:start] : @range[:regular]
   end
 
-  def valid_moves
-    moves = @movement.concat(attack_moves)
-    moves.each_with_object([]) do |direction, valid_moves|
-      valid_moves.concat(reach(direction))
-    end
+  def movement
+    @movement[:move].concat(attack_moves(@movement[:capture]))
   end
 
   def move(destination)
     super(destination)
     @first_move = false if @first_move
+  end
+end
+
+# King class
+class King
+  # returns arrays with all coordinates that are in reach of enemy pieces
+  def danger_zone
+    opponent_army.each_with_object([]) do |piece, danger_zone|
+      danger_zone.concat(piece.valid_moves)
+    end
+  end
+
+  # Option A
+  def reach(direction)
+    position = position()
+
+    (1..@range).each_with_object([]) do |_, reachable|
+      position = next_square(position, direction)
+      break unless valid_coord?(position)
+
+      target = @board.select_square(position)
+
+      next reachable << position if target.type == :empty && !danger_zone.include?(target)
+
+      reachable << position if target.enemy?
+      break
+    end
+  end
+
+  # adjusted reach(), deletes all coordinates that outs king into check
+  # Option B
+  def x_reach
+    super
+    danger_zone.each do |coord|
+      reachable.delete(coord)
+    end
+    reachable
   end
 end
