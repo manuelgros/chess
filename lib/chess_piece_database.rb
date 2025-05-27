57841fbf813450de901d1dd541af1aad584dc330
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-27 16:05:42 -0700

    Name changes for better comprehension

--- lib/chess_piece_database.rb
+++ lib/chess_piece_database.rb
@@ -3,20 +3,20 @@
 # PieceDatabase Module; stores information to create the various chess pieces
 module ChessPieceDatabase
   # Returns hash storing attributes for each Chess Piece; calling patter: pice_database[:piece_typ][:attribute]
-  # :moves is array with the direction the pieces can travel ([1, 0] => one up)
+  # :move_direction is array with the direction the pieces can travel ([1, 0] => one up)
   # :range decides how far a piece can go (7 = can potentially cross the entire board)
   def chess_piece_database # rubocop:disable Metrics/MethodLength
     {
       pawn: {
         amount: 8,
-        moves: {
+        move_direction: {
           white: {
-            regular: [[1, 0]],
-            capture_moves: [[1, 1], [1, -1]]
+            move: [[1, 0]],
+            capture: [[1, 1], [1, -1]]
           },
           black: {
-            regular: [[-1, 0]],
-            capture_moves: [[-1, 1], [-1, -1]]
+            move: [[-1, 0]],
+            capture: [[-1, 1], [-1, -1]]
           }
         },
         start_range: 2,
@@ -27,27 +27,27 @@ module ChessPieceDatabase
       },
       rook: {
         amount: 2,
-        moves: [[1, 0], [-1, 0], [0, 1], [0, -1]],
+        move_direction: [[1, 0], [-1, 0], [0, 1], [0, -1]],
         range: 7
       },
       knight: {
         amount: 2,
-        moves: [[2, 1], [-2, -1], [1, 2], [-1, -2], [1, -2], [2, -1], [-1, 2], [-2, 1]],
+        move_direction: [[2, 1], [-2, -1], [1, 2], [-1, -2], [1, -2], [2, -1], [-1, 2], [-2, 1]],
         range: 1
       },
       bishop: {
         amount: 2,
-        moves: [[1, 1], [1, -1], [-1, 1], [-1, -1]],
+        move_direction: [[1, 1], [1, -1], [-1, 1], [-1, -1]],
         range: 7
       },
       queen: {
         amount: 1,
-        moves: [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]],
+        move_direction: [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]],
         range: 7
       },
       king: {
         amount: 1,
-        moves: [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]],
+        move_direction: [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]],
         range: 1
       }
     }
