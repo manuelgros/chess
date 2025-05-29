2aaad2d053c518b9bc7b61742b53feef5e1b85f2
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-28 20:32:44 -0700

    Running full game

--- lib/main.rb
+++ lib/main.rb
@@ -7,11 +7,4 @@ require_relative '../lib/game'
 
 # Run code her
 game = Game.new
-# game.full_match
-white_pawn = game.board.select_square([1, 4])
-white_pawn.move([3, 4])
-black_pawn = game.board.select_square([6, 4])
-black_pawn.move([4, 4])
-pp white_pawn.position
-pp white_pawn.valid_moves
-pp white_pawn.save_moves
+game.full_match
