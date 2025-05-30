036eb12091dbb29281eac28fb3c69f51aa32b3f1
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-05-29 21:02:26 -0700

    Experimental implementation of en_passant rule

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -37,6 +37,19 @@ class Pawn < ChessPiece
     reachable
   end
 
+  def double_step?(destination)
+    destination[0] == position[0] + 2
+  end
+
+  def move(destination)
+    if double_step?(destination)
+      jumped_coord = [destination[0], (destination[1] - 1)]
+      jumped_square = board.select_square(jumped_coord)
+      jumped_square.en_passant == true if jumped_square.type == :empty
+    end
+    super(destination)
+  end
+
   # returns array with coordinated if target is enemy
   def attack_moves(attack_direction)
     attack_direction.select do |direction|
@@ -44,6 +57,7 @@ class Pawn < ChessPiece
       next unless board.includes_coordinates?(destination)
 
       target = @board.select_square(destination)
+
       enemy?(target)
     end
   end
