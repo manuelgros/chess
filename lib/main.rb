eb9b6e1623c020298030eca3ba7151a9e650feb5
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-06-03 15:34:32 -0700

    Debugged: Pawns were altering the original movement variables instead of just using them to return movement

--- lib/main.rb
+++ lib/main.rb
@@ -8,3 +8,17 @@ require_relative '../lib/game'
 # Run code her
 game = Game.new
 game.full_match
+
+# pp game.board.select_square([1, 1]).movement
+# game.board.select_square([6, 1]).move([2, 2])
+# puts game.board.select_square([2, 2]).class.name
+# pp game.board.select_square([1, 1]).movement
+# game.board.select_square([2, 2]).move([6, 1])
+# puts game.board.select_square([2, 2]).class.name
+# pp game.board.select_square([1, 1]).movement
+# game.board.select_square([6, 1]).move([2, 2])
+# puts game.board.select_square([2, 2]).class.name
+# pp game.board.select_square([1, 1]).movement
+# game.board.select_square([2, 2]).move([6, 1])
+# puts game.board.select_square([2, 2]).class.name
+# pp game.board.select_square([1, 1]).movement
