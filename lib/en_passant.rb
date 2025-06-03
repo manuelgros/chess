378b247d3542a2b65549a09f45b7ca25c72f5be9
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-06-03 15:53:05 -0700

    Debugged

--- lib/en_passant.rb
+++ lib/en_passant.rb
@@ -17,7 +17,7 @@ end
 # Submodule for EnPassant, including all methods in Pawn class
 module EnPassantPawn
   def double_step?(destination)
-    destination[0] == (position[0] + 2)
+    (destination[0] - position[0].abs == 2)
   end
 
   # method changes the color of EmptySquare object that is being over jumped to own color. It will then
