254eb7d8ed3608394b56bd36a5c961e8fbba4764
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-06-24 14:01:23 -0700

    Moved message into different group

--- lib/game_communication.rb
+++ lib/game_communication.rb
@@ -28,7 +28,8 @@ To select a piece you want to move, simply use its coordinates on the chess boar
       coord_input_error: 'Invalid input. Please select a valid command or a valid board coordinate (example: 14)',
       invalid_destination: 'The target you selected is invalid. Please select a different square on the board',
       invalid_selection: "This selection is invalid or there are no moves available for this piece. Please select a different piece for your move.\n\n",
-      capitulate: "#{current_player.player_name} has given up. #{current_player.opponent.player_name} has won!"
+      capitulate: "#{current_player.player_name} has given up. #{current_player.opponent.player_name} has won!",
+      new_turn: "\n#{current_player.player_name}, it is your turn.".yellow.bold
     }[message]
   end
 
@@ -37,12 +38,6 @@ To select a piece you want to move, simply use its coordinates on the chess boar
       piece_moves: "Selected piece: #{(piece.color.to_s.capitalize + piece.type.to_s.capitalize).green}\nMoves: #{piece.save_moves.to_s.green}",
       move: "#{(piece.color.to_s.capitalize + piece.type.to_s.capitalize).green} moves to #{piece.position[0].to_s.green} | #{piece.position[1].to_s.green}\n",
       invalid_move: 'Invalid move for'.red + " #{piece.color.to_s.capitalize + piece.type.to_s.capitalize}".green + '.'.red
-    }[message]
-  end
-
-  def game_messages(message)
-    {
-      new_turn: "\n#{@current_player.player_name}, it is your turn.".yellow.bold
     }[message]
   end
 
