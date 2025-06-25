886e5bf390d56881993f787252cec1827ee1e1b5
manuelgros <131806742+manuelgros@users.noreply.github.com>
2025-06-25 13:03:46 -0700

    Testing code

--- lib/main.rb
+++ lib/main.rb
@@ -7,9 +7,12 @@ require_relative '../lib/game'
 
 # Run code her
 Game.start_game
+# game = Game.new
 
 # ====== Pawn Promote Test =========
-# game.board.select_square([1, 1]).move([6, 1])
+# game.board.select_square([0, 3]).move([6, 3])
 # game.board.change_square([7, 1], EmptySquare.new(game.board))
+# puts game.board.select_square([6, 1]).promote?
 # game.board.select_square([6, 1]).move([7, 1])
 # puts game.board.select_square([7, 1]).type
+# game.full_match
